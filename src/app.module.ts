3126408f04661d45cf5cc74f001fb5d6d2d386db
caiya <caiya928@aliyun.com>
2018-02-02 17:04:57 +0800

    修改auth中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,40 @@
 import { Module, NestModule, MiddlewaresConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
-
 import { CatsModule } from './cats/cats.module';
-
 import { LoggerMiddleware, loggerMiddleware } from './common/middlewares/logger.middleware'
 import { CatsController } from './cats/cats.controller';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { PhotosModule } from './photos/photos.module';
-
-import { MongooseModule} from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { GraphQLModule, GraphQLFactory } from '@nestjs/graphql';
+import { graphqlExpress } from 'apollo-server-express';
 
 // 
 @Module({
-  imports: [TypeOrmModule.forRoot(), MongooseModule.forRoot('mongodb://192.168.20.5/test'), AuthModule ,CatsModule, PhotosModule],
+  imports: [TypeOrmModule.forRoot(), MongooseModule.forRoot('mongodb://192.168.20.5/test'), AuthModule, CatsModule, PhotosModule, GraphQLModule],
   controllers: [AppController],
   components: [],
 })
 export class ApplicationModule implements NestModule {
-  
-  // constructor(private readonly connection: Connection){};
+
+  constructor(private readonly graphQLFactory: GraphQLFactory) { };
+
 
   configure(consumer: MiddlewaresConsumer): void {
-    // consumer.apply(LoggerMiddleware).forRoutes({
-    //   path: '/cats', method: RequestMethod.GET
-    // }, {
-    //   path: '/cats', method: RequestMethod.POST
-    // })
 
+    // 添加日志中间件
     consumer.apply(LoggerMiddleware).with('ApplicationModule').forRoutes(CatsController);
     consumer.apply(loggerMiddleware).forRoutes(CatsController);
 
+    // 添加graphql中间件
+    const typeDefs = this.graphQLFactory.mergeTypesByPaths('./**/*.graphql');   // 加载graphql定义文件
+    const schema = this.graphQLFactory.createSchema({ typeDefs });
+    consumer.apply(graphqlExpress(req => ({ schema, rootValue: req }))).forRoutes({
+      path: '/graphql',
+      method: RequestMethod.ALL
+    });
     // path: '/cats', method: RequestMethod.ALL
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,8 @@ import { RequestMethod, Module, NestModule, MiddlewaresConsumer } from '@nestjs/
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
+import { CatsController } from '../cats/cats.controller';
+import { PhotosController } from '../photos/photos.controller';
 
 @Module({
   components: [AuthService, JwtStrategy],
@@ -11,10 +13,10 @@ import { JwtStrategy } from './jwt.strategy';
 export class AuthModule implements NestModule {
 
   public configure(consumer: MiddlewaresConsumer) {
-    consumer.apply(passport.authenticate('jwt', { session: false })).forRoutes({
-      path: '/auth/authorized',
-      method: RequestMethod.ALL
+    consumer.apply(passport.authenticate('jwt', { session: false })).forRoutes(CatsController, PhotosController, {
+      path: '/auth/authorized',    // 拦截所有url
+      method: RequestMethod.GET
     });
   }
-  
+
 }
